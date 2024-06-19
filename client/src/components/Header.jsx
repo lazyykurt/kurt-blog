0024cb618ef32244bf957e16b9f205d12c7b4364
lazyykurt <lazyyykurt@gmail.com>
2024-06-19 20:43:42 +0800

    完善Header组件 还有一个小bug为解决，登录按钮在，MenuBar的左侧，本应该放在右侧

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,81 +1,165 @@
-import { Avatar, Dropdown, Navbar, TextInput } from "flowbite-react";
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { Link } from "react-router-dom";
-import { useLocation } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { MdOutlineLightMode, MdOutlineDarkMode } from "react-icons/md";
+
+const userData = {
+  userName: "Kurt Zhang",
+  email: "example@email.com",
+  profilePicture: "/user/profile.jpg",
+};
 
 function Header() {
-  const location = useLocation();
-  const isActive = [
-    location.pathname === "/",
-    location.pathname === "/about",
-    location.pathname === "/projects",
-  ];
+  const path = useLocation().pathname;
+
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const handleSignIN = () => setIsSignedIn(!isSignedIn);
 
   return (
     <Navbar className="border-b-2" fluid rounded>
-      <Navbar.Brand href="http://localhost:5173/">
-        <img
-          src="../../public/favicons/android-chrome-192x192.png"
-          className="mr-3 h-6 sm:h-9"
-          alt="Kurt's Blog Logo"
-        />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-          Flowbite React
-        </span>
-      </Navbar.Brand>
-      <form>
-        <TextInput
-          type="text"
-          placeholder="Search..."
-          className="hidden md:block w-64"
-          rightIcon={AiOutlineSearch}
-        />
-      </form>
-      <UserPlace />
-      <Navbar.Collapse>
-        <Navbar.Link active={isActive[0]}>
-          <Link to="/"> Home </Link>
-        </Navbar.Link>
-        <Navbar.Link active={isActive[1]}>
-          <Link to="/about"> About </Link>
-        </Navbar.Link>
-        <Navbar.Link active={isActive[2]}>
-          <Link to="/projects"> Projects </Link>
-        </Navbar.Link>
-      </Navbar.Collapse>
+      <div className="flex items-center">
+        <Toggle />
+        <Logo />
+      </div>
+      <SeachBar />
+      {isSignedIn ? (
+        <UserMenu userData={userData} />
+      ) : (
+        <SignINBtn handleSignIN={handleSignIN} />
+      )}
+      <MenuBar path={path} />
     </Navbar>
   );
 }
 
 export default Header;
 
-function UserPlace() {
+function Toggle() {
   return (
-    <div className="flex md:order-2">
+    <div className="lg:hidden md:hidden">
+      <Navbar.Toggle />
+    </div>
+  );
+}
+
+function Logo() {
+  return (
+    <Navbar.Brand href="http://localhost:5173/">
+      <img
+        src="/favicons/android-chrome-192x192.png"
+        className="mr-1 h-8 sm:h-10"
+        alt="Kurt's Blog Logo"
+      />
+      <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+        Kurt&apos;s Blog
+      </span>
+    </Navbar.Brand>
+  );
+}
+
+function SeachBar() {
+  return (
+    <>
+      <form>
+        <TextInput
+          type="text"
+          placeholder="Search..."
+          className="hidden lg:block md:block w-64"
+          rightIcon={AiOutlineSearch}
+        />
+      </form>
+
+      <Button className="w-10 h-10 lg:hidden md:hidden" color="gray">
+        <AiOutlineSearch className="w-5 h-5" />
+      </Button>
+    </>
+  );
+}
+
+function MenuBar({ path }) {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [windowWidth]);
+
+  const isNotSm = !(windowWidth <= 768);
+
+  return (
+    <Navbar.Collapse>
+      <Navbar.Link active={path === "/"}>
+        <Link to="/"> Home </Link>
+      </Navbar.Link>
+      <Navbar.Link active={path === "/about"}>
+        <Link to="/about"> About </Link>
+      </Navbar.Link>
+      <Navbar.Link active={path === "/projects"}>
+        <Link to="/projects"> Projects </Link>
+      </Navbar.Link>
+      {isNotSm && <ThemeSwitch />}
+    </Navbar.Collapse>
+  );
+}
+
+function ThemeSwitch() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  return (
+    <Navbar.Link>
+      <button>
+        <a onClick={() => setIsDarkMode(!isDarkMode)}>
+          {isDarkMode ? (
+            <MdOutlineDarkMode className="w-5 h-5 " />
+          ) : (
+            <MdOutlineLightMode className="w-5 h-5" />
+          )}
+        </a>
+      </button>
+    </Navbar.Link>
+  );
+}
+
+function SignINBtn({ handleSignIN }) {
+  return (
+    <div className="d-md-block">
+      <Button onClick={handleSignIN} pill>
+        登录
+      </Button>
+    </div>
+  );
+}
+
+function UserMenu({ userData }) {
+  return (
+    <div className="flex md:order-2 d-md-block">
       <Dropdown
         arrowIcon={false}
         inline
         label={
-          <Avatar
-            alt="User settings"
-            img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-            rounded
-          />
+          <Avatar alt="User settings" img={userData.profilePicture} rounded />
         }
       >
         <Dropdown.Header>
-          <span className="block text-sm">Bonnie Green</span>
+          <span className="block text-sm">{userData.userName}</span>
           <span className="block truncate text-sm font-medium">
-            name@flowbite.com
+            {userData.email}
           </span>
         </Dropdown.Header>
-        <Dropdown.Item>Dashboard</Dropdown.Item>
+        <Dropdown.Item>
+          <Link to="/dashboard">Dashboard</Link>
+        </Dropdown.Item>
         <Dropdown.Item>Settings</Dropdown.Item>
         <Dropdown.Item>Earnings</Dropdown.Item>
         <Dropdown.Divider />
         <Dropdown.Item>Sign out</Dropdown.Item>
       </Dropdown>
-      <Navbar.Toggle />
     </div>
   );
 }
